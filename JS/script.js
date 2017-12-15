d12e6e2413321379bb4723c2c543643f9baee0a9
Juan Jose Elizalde <Juanjo@MacBook-Air-de-Juanjo.local>
2017-12-15 15:46:27 +0100

    add javascript file into html

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,35 @@
+var list = [
+  ['Adrian', 'guapo'],
+  ['Victor', 'feo'],
+];
+var listhtml;
+var names;
+var beauty;
+var text;
 
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+for (i = 0; i < list.length; i+= 1) {
+  var names = list[i][0];
+  var beauty = list [i][1];
+}
+
+function buildList(arr) {
+  var listhtml = '<ul>';
+    for (i = 0; i < names.length; i+= 1) {
+      listhtml += '<li>' + names + '</li>';
+    }
+    for (i = 0; i < beauty.length; i+= 1) {
+      listhtml += '<li>' + beauty + '</li>';
+    }
+    listhtml += '</ul>';
+    return listhtml;
+}
+
+
+html = "Your name is " + names + " you are ";
+html += buildList(text);
+print(html);
