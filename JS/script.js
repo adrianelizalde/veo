84f09986a142dccfc4c9010f97c722b2a052e314
Adrian Elizalde <Adrian@MacBook-Pro-de-Adrian.local>
2017-12-31 13:32:21 +0100

    Update javascript

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,8 @@ var list = [
 ];
 
 var listhtml;
-var names;
-var beauty;
+var names = [];
+var beauty = [];
 var outputDiv;
 var nameSelect;
 
@@ -15,8 +15,8 @@ function print(message) {       // why (message)
 }
 
 for (var i = 0; i < list.length; i += 1) {
-  names = list[i][0];
-  beauty = list [i][1];
+  names.push(list[i][0]);
+  beauty.push(list [i][1]);
   // if (names === 'Lozano') {
   //   nameSelect.push('Lozano');
   // } else {
@@ -29,10 +29,10 @@ for (var i = 0; i < list.length; i += 1) {
 function buildList(arr) {   // why (arr)
   var listhtml = '<ul>';
     for (var i = 0; i < names.length; i += 1) {
-      listhtml += '<li>' + names + '</li>';
+      listhtml += '<li>' + names[i] + '</li>';
     }
     for (i = 0; i < beauty.length; i += 1) {
-      listhtml += '<li>' + beauty + '</li>';
+      listhtml += '<li>' + beauty[i] + '</li>';
     }
     listhtml += '</ul>';
     return listhtml;
